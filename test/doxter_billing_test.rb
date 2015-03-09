daf328ca8e4c4b3c16f862f82cdc81f5e571a393
Andre Rauschenbach <a.rauschenbach@doxter.de>
2015-03-09 15:14:34 +0100

    Fix Test Naming convention

--- test/doxter_billing_test.rb
+++ test/doxter_billing_test.rb
@@ -1,6 +1,6 @@
 require './test/minitest_helper'
 
-class TestDoxterBilling < Minitest::Test
+class DoxterBillingTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::DoxterBilling::VERSION
   end
